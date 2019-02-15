184e6ad6f0e68810c377431d6d68fa4088c053c3
Amani Ibrahim <amaniibrahim@Amanis-MacBook-2.local>
2019-02-15 17:33:50 -0500

    comments for watchtower

--- cmd/watchtower/watches.go
+++ cmd/watchtower/watches.go
@@ -6,4 +6,10 @@ type Watch struct {
 	Network     string `json:"network"`
 	ClientTime  int64  `json:"clientTime"`
 	ServerTime  int64  `json:"serverTime"`
+	PatientId   string `json:"patientId"`
+	active      bool   `json:"patientId"`
+
+	// watchId should be unique (not in db on watch creation)
 }
+
+// on replacement of patient watch, set watch record to inactive before overwriting patient's watchId
